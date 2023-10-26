93a6e66f0b2f3c6a3ddd484e50b7fa40e56101d9
pedram.abdzadeh <pedram.abdzadeh@dfreight.org>
2023-10-26 15:39:18 +0400

    feat: top bar implementation and some initial configurations

---
+++ src/components/index.tsx
@@ -0,0 +1,2 @@
+export { default as TopBar } from './top-bar/top-bar';
+export { default as UserProfile } from './user-profile/user-profile';
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'yekan': ['IRANYekanX VF', 'tahoma']
       },
+      colors: {
+        karnameh: '#27AE60',
+        main: '#199DA3'
+      }
+
     },
   },
   plugins: [],
